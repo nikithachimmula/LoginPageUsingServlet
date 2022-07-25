a2fc4889eabba947348b1b87f5a2f0a381a61e08
nikitha chimmmula <nikithachimmula97@gmail.com>
2022-07-25 13:41:55 +0530

    PredefinedServlet.java

--- src/main/java/com/Registration/PredefinedServlet.java
+++ src/main/java/com/Registration/PredefinedServlet.java
@@ -1,2 +1,40 @@
-package com.Registration;public class PredefinedServlet {
-}
+package com.Registration;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+  @WebServlet(
+            description="Login Servlet Testing",
+            urlPatterns={ "/PredefinedServlet" },
+            initParams = {
+          @WebInitParam(name = "user", value = "Nikitha"),
+          @WebInitParam(name = "password", value = "Nicky@987")
+  }
+  )
+
+    public class PredefinedServlet extends HttpServlet {
+
+      @Override
+      protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          String userIdInput = req.getParameter("user");
+          String passwordInput = req.getParameter("password");
+
+          if (userIdInput.matches("User") && passwordInput.matches("password")) {
+              req.setAttribute("user", userIdInput);
+              req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
+          } else {
+              RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+              PrintWriter out = resp.getWriter();
+              out.println("<font color = red> Either username or password is wrong</font>");
+              rd.include(req, resp);
+          }
+      }
+  }
+
